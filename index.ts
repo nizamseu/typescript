1023914dc8491474e18856932973dbaafff9e62f
Nizam Uddin Babu <43912849+nizamseu@users.noreply.github.com>
2023-08-20 02:09:30 +0600

    union

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
-const courseName: string = "name Nizam2";
-console.log(courseName);
+type NoobDeveloper = {
+  name: string;
+};
+
+type JuniorDeveloper = NoobDeveloper & {
+  experience: number;
+  experties: string;
+};
+type NextLevelDeveloper = JuniorDeveloper & {
+  leaderShipExperience: number;
+  level: "mid" | "junior" | "senior";
+};
+
+const newDeveloper: NextLevelDeveloper = {
+  name: "nizam uddin",
+  experience: 10,
+  experties: "JS",
+  leaderShipExperience: 10,
+  level: "mid",
+};
+console.log(newDeveloper);
