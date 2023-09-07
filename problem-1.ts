bdc906ee495dce8b6c8f524d7c0a267bdcc8bbee
nizamseu <nizamcse.seu@gmail.com>
2023-09-07 15:48:19 +0600

    problem 1

---
+++ problem-1.ts
@@ -0,0 +1,8 @@
+function stringRepeater(string: string, number: number = 3): void {
+  for (let i = 0; i < number; i++) {
+    console.log(string);
+  }
+}
+
+stringRepeater("hello! Bangladesh", 1);
+stringRepeater("hi! Bangladesh");
